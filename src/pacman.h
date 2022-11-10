1e26ba8553c6d85926076e5a32cb08c493af4a0d
MakWanSing <114643412+MakWanSing@users.noreply.github.com>
2022-11-10 15:58:15 +0800

    Update pacman.h

---
+++ src/pacman.h
@@ -0,0 +1,14 @@
+#ifndef PACMAN
+#define PACMAN
+
+
+class PACMAN{
+  public:
+    int currentX,currentY;
+    int xVelocity=0,yVelocity=0;
+    
+    void.initpacman();
+    void.move();
+    void.changeDirection();
+};
+#endif
