912f2c7fe2cc37aa2b3a92230ec7c0660beab0bd
MakWanSing <114643412+MakWanSing@users.noreply.github.com>
2022-11-11 18:40:05 +0800

    Update screenrenderer.cpp
    conio.h --> ncurses.h

--- src/screenrenderer.cpp
+++ src/screenrenderer.cpp
@@ -1,14 +1,17 @@
 // Receive and convert keyboard input into move and decide further actions
-void ScreenRenderer::takeKeyboardInput(){
+void ScreenRenderer::KeyboardMode(){
     /* Takes keyboard input without blocking or delay */
-    if (_kbhit())
-        return _getch();
-    else
-        return -1;
+    initscr(); // 
+    noecho(); // no input display
+    nodelay(stdscr,true); // no blocking or delay
+    keypad(stdscr,true) // allow arrow key
 }
 
 string ScreenRenderer::takeCmdInput(string message){
     cout << message << endl;
     cin >> x;
     return x
+}
+string ScreenRenderer::CmdMode(){
+    endwin();
 }
