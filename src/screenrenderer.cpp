b30c0203d97cff7687d55124e37439d845b9e969
MakWanSing <114643412+MakWanSing@users.noreply.github.com>
2022-11-11 22:05:33 +0800

    Update screenrenderer.cpp

--- src/screenrenderer.cpp
+++ src/screenrenderer.cpp
@@ -1,21 +1,21 @@
 // Receive and convert keyboard input into move and decide further actions
 void ScreenRenderer::KeyboardModeOpen(){ // in keyboardmode, use printw(mystring.c_str()) instead of cout<< mystring
-
     initscr(); //initialize screen
     noecho(); // no input display
 }
 void ScreenRenderer::KeyboardModeWb(){
-    
     nodelay(stdscr,false); /* Takes keyboard input with blocking or delay */
     keypad(stdscr,false) // not allow arrow key
 }
 void ScreenRenderer::KeyboardModeNb(){
-
     nodelay(stdscr,true); /* Takes keyboard input without blocking or delay */
     keypad(stdscr,true) // allow arrow key
 }
 void ScreenRenderer::KeyboardModeClose(){
     endwin();
+}
+void ScreenRenderer::KeyboardModePrint(string str){
+    printw(str.c_str());
 }
 
 string ScreenRenderer::takeCmdInput(string message){
