3c563b40e9ff79e21b2d59de93bc2db9eca7a58a
MakWanSing <114643412+MakWanSing@users.noreply.github.com>
2022-11-17 00:59:42 +0800

    Update pacman.h

--- src/pacman.h
+++ src/pacman.h
@@ -4,10 +4,13 @@
 
 class Pacman{
   public:
-    void changeDirection(int x,int y);
+    Pacman(int spawnX, int spawnY);
+    void setVelocity(int x,int y);
+    void setPosition(int x,int y);
     int* getVelocity();
     int* getPosition();
-    Pacman(int spawnX, int spawnY);
+    
+  
   private:
     int* position;
     int* velocity;
