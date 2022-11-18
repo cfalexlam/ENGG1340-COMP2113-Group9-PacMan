f6fde959206cfd1f010457a780b003ce69c61a98
MakWanSing <114643412+MakWanSing@users.noreply.github.com>
2022-11-19 01:09:02 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <ncurses.h> // need to add "-lncurses" at last when compile
 #include <iostream>
+#include <unistd.h>
 
 #include "player.h"
 #include "maze.h"
@@ -24,38 +25,37 @@ void MainGame::mainLoop(){
     Player pl(s.takeCmdInput("Please enter your name: "));
 
     Maze m(s.takeCmdInput("Please enter the filename of the maze: "));
-
+    Pacman p;
     s.takeCmdInput("Hi " + p.getName() + "! Are you ready? (Press any key to continue)");
-    
     s.KeyboardModeOpen();
-	s.KeyboardModeWb();
+    s.KeyboardModeWb();
     getch();
-	s.KeyboardModeNb();
+    s.KeyboardModeNb();
 	
     while(1){
         m.printmaze();
         int input;
         input = getch();
+		if (input == int('q'))
+			break;
         switch(input)
         {
             case KEY_UP:
-                m.p.moveup();
+            	p.setVelocity(-1,0);
                 break;
             case KEY_DOWN;
-                m.p.movedown;
+                p.setVelocity(1,0);
                 break;
             case KEY_LEFT;
-                m.p.moveleft();
+                p.setVelocity(0,-1);
                 break;
             case KEY_RIGHT;
-                m.p.moveright();
+                p.setVelocity(0,1);
                 break;
-            case int('q');
         }
 		
         // check collisions of ghosts and pacman before updating the screen
         if (isLosing(m)){
-
             pl.loseLife();
 
             if (pl.getLives == 0){
@@ -69,21 +69,30 @@ void MainGame::mainLoop(){
             m.respawn(newLevel=false); // restore the starting positions of ghosts and pacman, while keeping the dots at their current places
             s.printRespawnCountdown(); // countdown for 3 seconds before restarting the game
         }
-
+		
+		if (! m.isWall(p.getPosition()[0]+p.getVelocity()[0],p.getPosition()[1]+p.getVelocity()[1]))
+		{
+			if(m.isFood(p.getPosition()[0]+p.getVelocity()[0],p.getPosition()[1]+p.getVelocity()[1]))
+			{
+				p1.updateScore(); // increase the player's score if a dot is eaten
+			}
+			m.movepacman();
+			p.setPosition( p.getPosition()[0]+p.getVelocity()[0] , p.getPosition()[1]+p.getVelocity()[1] );
+		
+		}
+		
         m.updatePos(); // update the positions of dots, ghosts and pacman on the maze
         m.updateStates(); // update the states of ghosts and pacman on the maze
-        pl.updateScore();  // increase the player's score if a dot is eaten
 
         if (!m.getDotsPos()){
             pl.updateCurrentLevel();
             m.respawn(newLevel=true);
             s.printRespawnCountdown();
         }
-
-        s.updateScreen();  // print new maze to screen
+		
+		usleep(0.2*1000000); // 0.2s delay
     }
-
-
+	s.keyboardModeClose();
 }
 
 
