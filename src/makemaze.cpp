a2a011e4c8cd8058b8a236ba48c9cdedf2b31792
Bosco <gtw0704@connect.hku.hk>
2022-12-01 20:24:22 +0800

    correct identation

--- src/makemaze.cpp
+++ src/makemaze.cpp
@@ -48,7 +48,7 @@ int makemaze(){
 	do
 	{
         
-		clear();
+        clear();
         printw("Rules:\n1. Use up down left right to control the place to insert character.\n");
         printw("2. Type G, X, O and = to insert ghosst, player, pellets and walls respectively.\n");
         printw("3. Do not insert more than 1 player.\n");
